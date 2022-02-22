3d2fefe82de8a7693079c6d405c8b796b5dfa989
NotPerr <83740861+NotPerr@users.noreply.github.com>
2022-02-22 11:35:06 +0800

    first rn

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={styles.image}
+        source={{
+          uri: 'https://media.tenor.com/images/1c39f2d94b02d8c9366de265d0fba8a0/tenor.gif',
+        }}
+      />
+
+      <Text style={styles.text}>胡芳瑜</Text>
+      <Text style={styles.text}>110919003</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#DAF7A6',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  text: {
+    color: '#006600',
+    fontSize: 40,
+    fontFamily: 'Verdana'
+  },
+  image: {
+    width: 200,
+    height: 200  
+  }
 });
